cb823572d697fb93746038f0f5e8b2af1d302518
jackwhelan <jackw97@live.ie>
2020-01-04 15:47:26 +0000

    Added basic ebay spider

---
+++ spider.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+def ebay_spider(max_pages):
+    page = 0
+    while page < max_pages:
+        page += 1
+        url = 'https://www.ebay.ie/b/Internal-Hard-Disk-Drives/56083/bn_780224?rt=nc&_pgn=' + str(page)
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text, features="html.parser")
+        for link in soup.findAll('a', {'class':'s-item__link'}):
+            href = link.get('href')
+            print(href)
+
+ebay_spider(1)
